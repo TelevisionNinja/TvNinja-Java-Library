4369b5de8e7b0210f28be10b4f7c618d09b43619
TelevisionNinja <TelevisionNinja@gmail.com>
2025-10-08 02:54:23 -0400

    updated unit test functions

--- src/televisionninja/lib/unittest/UnitTest.java
+++ src/televisionninja/lib/unittest/UnitTest.java
@@ -1,14 +1,16 @@
 package televisionninja.lib.unittest;
 
+import java.util.Objects;
+
 public class UnitTest {
-	public static void UNIT_TEST_EQ(boolean actual, boolean expected) {
-        if (actual != expected) {
+	public static <T> void UNIT_TEST_EQ(T actual, T expected) {
+        if (!Objects.equals(actual, expected)) {
             throw new AssertionError("Failed UNIT_TEST_EQ\nValue:\n" + actual + "\n\nExpected:\n" + expected + "\n\n");
         }
     }
 
-    public static void UNIT_TEST_NEQ(boolean actual, boolean notExpected) {
-        if (actual == notExpected) {
+    public static <T> void UNIT_TEST_NEQ(T actual, T notExpected) {
+        if (Objects.equals(actual, notExpected)) {
             throw new AssertionError("Failed UNIT_TEST_NEQ\nValue:\n" + actual + "\n\nNot Expected:\n" + notExpected + "\n\n");
         }
     }
